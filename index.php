d6b0e18113f3e63e4c409270d557249c07270049
JeffWDH <JeffWDH@users.noreply.github.com>
2021-12-13 10:33:09 -0500

    Added hidden mode

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ $font_size = 36; // Font size (1-300)
 $font_pad = 25;  // Number of pixels to pad the image around the text
 $frame_thickness = 5; // Frame thickness in pixels
 $number_format = 1; // 0 = off, 1 = add commas to number
+$hidden = 0; // 0 = off, 1 = send 1x1 transparent imag
 
 // Avoid counting repeated visists within $cookie_expiration seconds
 $cookie_expiration = 60;
@@ -70,6 +71,14 @@ if (isset($_GET['number_format'])) {
   }
 }
 
+if (isset($_GET['hidden'])) {
+  if ($_GET['hidden'] == 1) {
+    $hidden = 1;
+  } else {
+    $hidden = 0;
+  }
+}
+
 if (isset($_GET['frame_thickness'])) {
   $frame_thickness = sanitizeInputNumber($_GET['frame_thickness']);
 }
@@ -95,55 +104,64 @@ if ($number_format == 1) {
   $count_string = $count;
 }
 
-// Determine text size
-$count_size = imagettfbbox($font_size, 0, $font_file, $count_string);
-$count_size_x = $count_size[4];
-$count_size_y = abs($count_size[5]);
+if ($hidden == 0) {
+  // Determine text size
+  $count_size = imagettfbbox($font_size, 0, $font_file, $count_string);
+  $count_size_x = $count_size[4];
+  $count_size_y = abs($count_size[5]);
 
-// Determine image size
-if ($frame_thickness == 0) {
-  $image_size_x = $count_size_x + $font_pad;
-  $image_size_y = $count_size_y + $font_pad;
+  // Determine image size
+  if ($frame_thickness == 0) {
+    $image_size_x = $count_size_x + $font_pad;
+    $image_size_y = $count_size_y + $font_pad;
+  } else {
+    $image_size_x = $count_size_x + $frame_thickness + $font_pad;
+    $image_size_y = $count_size_y + $frame_thickness + $font_pad;
+  }
+
+  // Create image object
+  $image = imagecreatetruecolor($image_size_x, $image_size_y);
+
+  // Image antialiasing
+  imageantialias($image, false);
+
+  // Define colors
+  // Black (RGB 0, 0, 0) is a special case as the object creation will return 0
+  // The rest will return >0 if successful
+
+  $background_color_obj = imagecolorallocate($image, $colors[$background_color][0], $colors[$background_color][1], $colors[$background_color][2]);
+  if (($background_color != "black") && ($background_color_obj == 0)) {
+    die("Cannot allocate background color.");
+  }
+
+  $font_color_obj = imagecolorallocate($image, $colors[$font_color][0], $colors[$font_color][1], $colors[$font_color][2]);
+  if (($font_color != "black") && ($font_color_obj == 0)) {
+    die("Cannot allocate font color.");
+  }
+
+  $frame_color_obj = imagecolorallocate($image, $colors[$frame_color][0], $colors[$frame_color][1], $colors[$frame_color][2]);
+  if (($frame_color != "black") && ($frame_color_obj == 0)) {
+    die("Cannot allocate frame color.");
+  }
+
+  // Fill background
+  if ($frame_thickness == 0) {
+    imagefilledrectangle($image, 0, 0, $image_size_x, $image_size_y, $background_color_obj);
+  } else {
+    imagefilledrectangle($image, 0, 0, $image_size_x, $image_size_y, $frame_color_obj);
+    imagefilledrectangle($image, ($frame_thickness/2), ($frame_thickness/2), $image_size_x-($frame_thickness/2), $image_size_y-($frame_thickness/2), $background_color_obj);
+  }
+
+  // Add the text
+  imagettftext($image, $font_size, 0, ((($image_size_x + $count_size_x) / 2) - $count_size_x), (($image_size_y + $count_size_y) / 2), $font_color_obj, $font_file, $count_string);
 } else {
-  $image_size_x = $count_size_x + $frame_thickness + $font_pad;
-  $image_size_y = $count_size_y + $frame_thickness + $font_pad;
+  // Create 1x1 transparent image
+  $image = imagecreatetruecolor(1, 1);
+  imagealphablending($image, false);
+  imagesavealpha($image, true);
+  $bgcolor = imagecolorallocatealpha($image, 0, 0, 0, 127);
+  imagefill($image, 0, 0, $bgcolor);
 }
-
-// Create image objct
-$image = imagecreatetruecolor($image_size_x, $image_size_y);
-
-// Image antialiasing
-imageantialias($image, false);
-
-// Define colors
-// Black (RGB 0, 0, 0) is a special case as the object creation will return 0
-// The rest will return >0 if successful
-
-$background_color_obj = imagecolorallocate($image, $colors[$background_color][0], $colors[$background_color][1], $colors[$background_color][2]);
-if (($background_color != "black") && ($background_color_obj == 0)) {
-  die("Cannot allocate background color.");
-}
-
-$font_color_obj = imagecolorallocate($image, $colors[$font_color][0], $colors[$font_color][1], $colors[$font_color][2]);
-if (($font_color != "black") && ($font_color_obj == 0)) {
-  die("Cannot allocate font color.");
-}
-
-$frame_color_obj = imagecolorallocate($image, $colors[$frame_color][0], $colors[$frame_color][1], $colors[$frame_color][2]);
-if (($frame_color != "black") && ($frame_color_obj == 0)) {
-  die("Cannot allocate frame color.");
-}
-
-// Fill background
-if ($frame_thickness == 0) {
-  imagefilledrectangle($image, 0, 0, $image_size_x, $image_size_y, $background_color_obj);
-} else {
-  imagefilledrectangle($image, 0, 0, $image_size_x, $image_size_y, $frame_color_obj);
-  imagefilledrectangle($image, ($frame_thickness/2), ($frame_thickness/2), $image_size_x-($frame_thickness/2), $image_size_y-($frame_thickness/2), $background_color_obj);
-}
-
-// Add the text
-imagettftext($image, $font_size, 0, ((($image_size_x + $count_size_x) / 2) - $count_size_x), (($image_size_y + $count_size_y) / 2), $font_color_obj, $font_file, $count_string);
 
 // Send picture to client
 header("Content-Type: image/png");
